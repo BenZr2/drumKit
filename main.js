514148abdc27b70d06ec3512277eaaa672ed882f
Ben Reitz <ben.reitz2@googlemail.com>
2021-02-27 21:45:18 +0100

    audio.currentTime to 0 to play keys multiple times

--- main.js
+++ main.js
@@ -13,31 +13,41 @@ const aKey = document.getElementById('A');
 
 
 document.addEventListener('keydown', function(event) {
+
     if (event.key === 'a') {
+        audioA.currentTime = 0;
         audioA.play();
     }
     else if (event.key === 's') {
+        audioS.currentTime = 0;
         audioS.play();
     }
     else if (event.key === 'd') {
+        audioD.currentTime = 0;
         audioD.play();
     }
     else if (event.key === 'f') {
+        audioF.currentTime = 0;
         audioF.play();
     }
     else if (event.key === 'g') {
+        audioG.currentTime = 0;
         audioG.play();
     }
     else if (event.key === 'h') {
+        audioH.currentTime = 0;
         audioH.play();
     }
     else if (event.key === 'j') {
+        audioJ.currentTime = 0;
         audioJ.play();
     }
     else if (event.key === 'k') {
+        audioK.currentTime = 0;
         audioK.play();
     }
     else if (event.key === 'l') {
+        audioL.currentTime = 0;
         audioL.play();
     }
     
